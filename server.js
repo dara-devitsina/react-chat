99694275ec6c6a475fda8c524b8b3b327b4a9134
dara <dashadevitsina@gmail.com>
2021-05-21 08:28:55 +0300

    create server

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true,
+        transports: ['websocket']
+
+    }
+});
+
+
+const rooms = new Map([
+
+]);
+
+app.get('/rooms', function(req, res){
+	res.json(rooms);
+});
+
+// when user connects we get new variable socket that will store all user info
+io.on('connection', (socket) => {
+	console.log('user connected', socket.id);
+});
+
+
+server.listen(9999, (err) => {
+	if (err) {
+		throw Error(err);
+	}
+	console.log('Сервер запущен');
+})
+
