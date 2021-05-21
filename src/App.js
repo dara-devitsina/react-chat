6bcbe7312269ab8d7e93a03a2fd656253793b6f0
dara <dashadevitsina@gmail.com>
2021-05-21 09:33:56 +0300

    add styles, modify index.html

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ function App() {
     io('http://localhost:9999');
   };
 
-  return (
-    <div className="wrapper">
-      <button onClick={connectSocket}>CONNECT</button>
-      </div>
-  );
   // return (
-  // <div className="Wrapper">
-  //   <div class="join-block">
-  //     <input type="text" placeholder = "Your name" value = "" />
-  //     <button class="btn btn-success">JOIN</button>
-  //   </div>
-  //   </div>
-  // )
+  //   <div className="wrapper">
+  //     <button onClick={connectSocket}>CONNECT</button>
+  //     </div>
+  // );
+  return (
+  <div className="Wrapper">
+    <div class="join-block">
+      <input type="text" placeholder = "Room ID" value = "" />
+      <input type="text" placeholder = "Your name" value = "" />
+      <button class="btn btn-success">JOIN</button>
+    </div>
+    </div>
+  )
 }
 
 export default App;
